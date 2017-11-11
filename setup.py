b6cd328c0e0fd08a91a04f4d71fce23ad6b109da
Ho Wa Wong <howa.wong@gmail.com>
2017-11-11 10:14:53 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools.command.install import install
+from os import system
+from setuptools import setup
+import subprocess
+
+class MyInstall(install):
+    def run(self):
+        a = subprocess.check_output([
+            'apt-get install -y glibc'
+        ])
+        install.run(self)
+# in the setup function:
+cmdclass={'install': MyInstall}
+setup(cmdclass=cmdclass)
